d9f333586e888280aa0483f62098d6ef227b9fa2
minjun8946 <minjun8946@daum.net>
2021-07-31 04:12:17 +0900

    ⚡️ :: BasicPlayerInfo

---
+++ domain/src/main/java/com/example/domain/entity/BasicPlayerInfo.kt
@@ -0,0 +1,14 @@
+package com.example.domain.entity
+
+data class BasicPlayerInfo(
+
+    val personId : Int,
+
+    val firstName : String,
+
+    val lastName : String,
+
+    val teamId : Int,
+
+    val teamName : String
+)
