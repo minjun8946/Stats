99b63c60672287773a10b2ee7d715b293a507321
minjun8946 <minjun8946@daum.net>
2021-08-03 19:47:49 +0900

    🐛::GetAllPlayerResponse nullcheck

--- data/src/main/java/com/example/data/entity/StatsBasicInfoData.kt
+++ data/src/main/java/com/example/data/entity/StatsBasicInfoData.kt
@@ -4,15 +4,15 @@ import com.example.domain.entity.StatsBasicInfo
 
 
 data class GetAllPlayerResponse(
-    val data : List<BasicData>,
+    val data : List<BasicData>?,
 
-    val meta : MetaData
+    val meta : MetaData?
 )
 
 fun GetAllPlayerResponse.toEntity() =
     StatsBasicInfo(
-        data = data.map { it.toEntity() },
-        meta = meta.toEntity()
+        data = data?.map { it.toEntity() },
+        meta = meta?.toEntity()
     )
 
 
