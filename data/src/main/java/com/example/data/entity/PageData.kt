baddff5763ab2c426dd9f3614ab0998a0ec68d80
minjun8946 <minjun8946@daum.net>
2021-08-03 19:47:28 +0900

    🐛:: PageData nullcheck

--- data/src/main/java/com/example/data/entity/PageData.kt
+++ data/src/main/java/com/example/data/entity/PageData.kt
@@ -3,9 +3,9 @@ package com.example.data.entity
 import com.example.domain.entity.Page
 
 data class PageData(
-    val page : Int,
-    val perPage : Int,
-    val search : String
+    val page : Int?,
+    val perPage : Int?,
+    val search : String?
 )
 
 fun Page.toEntity() : PageData =
