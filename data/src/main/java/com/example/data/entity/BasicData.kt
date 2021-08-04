708834c19ae9dbed8f33ebcdeeb642976dfe0858
minjun8946 <minjun8946@daum.net>
2021-08-05 03:19:41 +0900

    ⚡️:: add player info

--- data/src/main/java/com/example/data/entity/BasicData.kt
+++ data/src/main/java/com/example/data/entity/BasicData.kt
@@ -6,9 +6,13 @@ import com.example.domain.entity.StatsBasicInfo
 import com.google.gson.annotations.SerializedName
 
 data class BasicData(
-    val id : Int?,
-    val first_name : String?,
-    val last_name : String?,
+    val id : Int,
+    val first_name : String,
+    val last_name : String,
+    val position : String,
+    val height_feet : Int,
+    val height_inches : Int,
+    val weight_pounds : Int,
     val team : BasicTeamData
 )
 
@@ -17,6 +21,10 @@ fun BasicData.toEntity() =
         personId= id,
         firstName = first_name,
         lastName = last_name,
+        position= position,
+        height_feet= height_feet,
+        height_inches = height_inches,
+        weight_pounds = weight_pounds,
         teamId = team.teamId,
         teamName = team.teamName
     )
--- domain/src/main/java/com/example/domain/entity/Basic.kt
+++ domain/src/main/java/com/example/domain/entity/Basic.kt
@@ -2,9 +2,13 @@ package com.example.domain.entity
 
 data class Basic (
 
-    val personId : Int?,
-    val firstName : String?,
-    val lastName : String?,
-    val teamId : Int?,
-    val teamName : String?
+    val personId : Int,
+    val firstName : String,
+    val lastName : String,
+    val position : String,
+    val height_feet : Int,
+    val height_inches : Int,
+    val weight_pounds : Int,
+    val teamId : Int,
+    val teamName : String
 )
