8f44a3a0e4e902950f5eff990fdd06db1627de68
minjun8946 <minjun8946@daum.net>
2021-08-08 02:25:26 +0900

    🐛:: change BasicTeamData

--- data/src/main/java/com/example/data/entity/BasicTeamData.kt
+++ data/src/main/java/com/example/data/entity/BasicTeamData.kt
@@ -6,7 +6,7 @@ import com.google.gson.annotations.SerializedName
 data class BasicTeamData(
     @SerializedName("id")
     val teamId : Int,
-    @SerializedName("full_name")
+    @SerializedName("name")
     val teamName : String
 )
 
