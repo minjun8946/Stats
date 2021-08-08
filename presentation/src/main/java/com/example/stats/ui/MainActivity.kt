b3a488e6c364a98e96619b48c5f634e9246e05a8
minjun8946 <minjun8946@daum.net>
2021-08-08 21:54:56 +0900

    ⚡️:: infinite scroll, search

--- presentation/src/main/java/com/example/stats/ui/MainActivity.kt
+++ presentation/src/main/java/com/example/stats/ui/MainActivity.kt
@@ -2,12 +2,12 @@ package com.example.stats.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.databinding.DataBindingUtil
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.viewpager2.widget.ViewPager2
 import com.example.stats.R
-import com.example.stats.adapter.MainAdapter
+import com.example.stats.base.SingleLiveEvent
 import com.example.stats.databinding.ActivityMainBinding
 import com.example.stats.model.PageModel
 import com.example.stats.viewmodel.MainViewModel
@@ -16,10 +16,12 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 class MainActivity : AppCompatActivity() {
 
     private val viewModel : MainViewModel by viewModel()
-    lateinit var binding : ActivityMainBinding
+    private lateinit var binding : ActivityMainBinding
 
     private lateinit var recyclerView: RecyclerView
+    private lateinit var searchEditText : EditText
 
+    private var searchText =""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,14 +30,43 @@ class MainActivity : AppCompatActivity() {
         binding.lifecycleOwner = this
         setContentView(binding.root)
 
+        searchEditText = findViewById(R.id.search_edittext)
         recyclerView = findViewById(R.id.recycler_view)
-        viewModel.getAllPlayer(PageModel(1,10,null))
+
+        getAllPlayer()
         successEvent()
+        scrollListenerEvent()
+        search()
     }
-    fun successEvent(){
+
+    private fun search(){
+        viewModel.searchEvent.observe(this,{
+            searchText = searchEditText.text.toString()
+            getAllPlayer()
+        })
+    }
+
+    private fun scrollListenerEvent(){
+        viewModel.scrollListenerEvent.observe(this,{
+            getAllPlayer()
+        })
+    }
+
+    private fun successEvent(){
         viewModel.successEvent.observe(this,{
             println("실행")
         })
     }
+
+    private fun getAllPlayer(){
+        viewModel.getAllPlayer(PageModel(viewModel.page,15,searchText))
+    }
+
+    fun <T> singleLiveEvent(event : SingleLiveEvent<T>) {
+        event.observe(this,{
+
+        })
+    }
+
 
 }
