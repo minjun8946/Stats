c8b45ab6c4d331282d5f96fdca59d07e3d4adf15
minjun8946 <minjun8946@daum.net>
2021-08-08 23:49:24 +0900

    ⚡️:: baseViewModel api execute

--- presentation/src/main/java/com/example/stats/base/BaseViewModel.kt
+++ presentation/src/main/java/com/example/stats/base/BaseViewModel.kt
@@ -1,8 +1,12 @@
 package com.example.stats.base
 
 import androidx.lifecycle.ViewModel
+import com.example.domain.base.UseCase
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
 
 open class BaseViewModel : ViewModel(){
 
@@ -10,6 +14,15 @@ open class BaseViewModel : ViewModel(){
 
     fun addDisposable(disposable: Disposable) {
         compositeDisposable.add(disposable)
+    }
+
+    fun <T,E>execute(data : T, disposableSingleObserver: DisposableSingleObserver<E>,useCase: UseCase<T,E>){
+        val disposable = useCase.create(data)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeWith(disposableSingleObserver)
+
+        addDisposable(disposable)
     }
 
     override fun onCleared() {
