83c693cbf1f3ff255588a7aacbbd5d5f51321979
minjun8946 <minjun8946@daum.net>
2021-08-08 21:52:59 +0900

    ⚡️:: scrollListener Binding Adapter

--- presentation/src/main/java/com/example/stats/bindingadapter/BindingAdapter.kt
+++ presentation/src/main/java/com/example/stats/bindingadapter/BindingAdapter.kt
@@ -1,16 +1,26 @@
 package com.example.stats.bindingadapter
 
-import android.widget.LinearLayout
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.viewpager2.widget.ViewPager2
 import com.example.stats.adapter.MainAdapter
+import com.example.stats.base.SingleLiveEvent
 
 
 @BindingAdapter("recyclerView")
 fun recyclerView(recyclerView: RecyclerView, adapter: MainAdapter) {
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(recyclerView.context,LinearLayoutManager.VERTICAL,false)
-        adapter.notifyDataSetChanged()
+}
+
+@BindingAdapter("scrollListener")
+fun scrollListener(recyclerView: RecyclerView, scrollListener : SingleLiveEvent<Unit>){
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener(){
+                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                        super.onScrolled(recyclerView, dx, dy)
+                        if(!recyclerView.canScrollVertically(1)){
+                                scrollListener.setValue(Unit)
+                        }
+                }
+        })
 }
