c91c2d72e0d2c1e50b85580d1b821d322ba9bce0
minjun8946 <minjun8946@daum.net>
2021-08-29 17:32:32 +0900

    ⚡️:: BaseActivity

---
+++ presentation/src/main/java/com/example/stats/base/BaseActivity.kt
@@ -0,0 +1,20 @@
+package com.example.stats.base
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<T : ViewDataBinding, R : BaseViewModel> : AppCompatActivity() {
+    lateinit var binding: T
+
+    abstract val layoutResID: Int
+    abstract val viewModel: R
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, layoutResID)
+        binding.lifecycleOwner = this@BaseActivity
+    }
+}
