ac3364ebe6c07e90e08533cf1ec0e91e7b90bde2
minjun8946 <minjun8946@daum.net>
2021-08-29 17:33:18 +0900

    ⚡️:: change baseactivity

--- presentation/src/main/java/com/example/stats/ui/PlayerDetailActivity.kt
+++ presentation/src/main/java/com/example/stats/ui/PlayerDetailActivity.kt
@@ -6,9 +6,12 @@ import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import com.example.data.entity.BasicData
 import com.example.domain.entity.Basic
 import com.example.stats.R
+import com.example.stats.base.BaseActivity
+import com.example.stats.base.BaseViewModel
 import com.example.stats.base.teamImage
 import com.example.stats.databinding.ActivityPlayerDetailBinding
 import com.example.stats.model.BasicModel
@@ -16,53 +19,35 @@ import com.example.stats.model.PlayerSeasonIdModel
 import com.example.stats.viewmodel.PlayerDetailViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class PlayerDetailActivity : AppCompatActivity() {
+class PlayerDetailActivity : BaseActivity<ActivityPlayerDetailBinding,BaseViewModel>() {
 
-    private val viewModel : PlayerDetailViewModel by viewModel()
-    lateinit var binding : ActivityPlayerDetailBinding
-
-    lateinit var name : TextView
-    lateinit var height : TextView
-    lateinit var weight : TextView
-    lateinit var teamName : TextView
-    lateinit var teamImage : ImageView
+    override val viewModel : PlayerDetailViewModel by viewModel()
+    override val layoutResID: Int = R.layout.activity_player_detail
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_player_detail)
-        binding.vm = viewModel
-        binding.lifecycleOwner = this
-        setContentView(binding.root)
-
-        
 
         val basicData = intent.getSerializableExtra("data") as BasicModel
         setBasicData(basicData)
 
         viewModel.setYear()
-        viewModel.getPlayerSeasonEvent.observe(this,{
-            if(it >1980)
-            viewModel.getPlayerSeasonStats(PlayerSeasonIdModel(it,basicData.personId))
-            println(it)
-        })
+        getPlayerSeasonEvent(basicData)
 
-        println(basicData)
     }
+
+    private fun getPlayerSeasonEvent(basicData: BasicModel){
+        viewModel.getPlayerSeasonEvent.observe(this,{
+            if(it >1980) viewModel.getPlayerSeasonStats(PlayerSeasonIdModel(it,basicData.personId))
+        })
+    }
+
 
     @SuppressLint("UseCompatLoadingForDrawables")
     private fun setBasicData(basicData: BasicModel){
-
-        name=findViewById(R.id.name)
-        name.text = basicData.name
-        height = findViewById(R.id.height)
-        height.text = basicData.height
-        weight = findViewById(R.id.weight)
-        weight.text = basicData.pounds
-        teamName = findViewById(R.id.team_name)
-        teamName.text = basicData.teamName
-        teamImage = findViewById(R.id.detail_team_image)
-        teamImage.setImageDrawable(resources.getDrawable(basicData.teamId))
-        println("${basicData.teamId} 아이더 넘버 ")
+        binding.name.text = basicData.name
+        binding.height.text = basicData.height
+        binding.weight.text = basicData.pounds
+        binding.teamName.text = basicData.teamName
+        binding.detailTeamImage.setImageDrawable(resources.getDrawable(basicData.teamId))
     }
 }
