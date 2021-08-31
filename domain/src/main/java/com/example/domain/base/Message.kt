92f498f6a4902791623c252823b147d74daaa3c0
minjun8946 <minjun8946@daum.net>
2021-09-01 08:23:56 +0900

    🐛:: base message

--- domain/src/main/java/com/example/domain/base/Message.kt
+++ domain/src/main/java/com/example/domain/base/Message.kt
@@ -2,17 +2,12 @@ package com.example.domain.base
 
 enum class Message {
     // network message
-    INVALID,
-    UNAUTHORIZED,
-    FORBIDDEN,
-    NOT_FOUND,
-    NOT_ACCEPTABLE,
-    CONFLICT,
-    PAYLOAD_TOO_LARGE,
-    UNKNOWN_ERROR,
-    SERVER_ERROR,
-    NETWORK_ERROR,
-
-    // search message
-    LAST_PAGE
+    BadRequest,
+    NotFound,
+    NotAcceptable ,
+    TooManyRequests,
+    InternalServerError,
+    ServiceUnavailable,
+    UnKnownError,
+    NetWorkError
 }
