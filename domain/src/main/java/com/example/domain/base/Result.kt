e12bb6faa48059b88421823952c163c10674ab74
minjun8946 <minjun8946@daum.net>
2021-09-01 08:24:38 +0900

    🐛:: result error

--- domain/src/main/java/com/example/domain/base/Result.kt
+++ domain/src/main/java/com/example/domain/base/Result.kt
@@ -2,7 +2,11 @@ package com.example.domain.base
 
 sealed class Result<T> {
 
-    data class Success<T>(val response : T) :Result<T>()
+    data class Success<T>(val response: T): Result<T>()
 
-    data class Error<T>(val response : T ?=null) : Result<T>()
+    data class Error<T>(
+        var response: T? = null,
+        var message: Message
+    ): Result<T>()
+
 }
