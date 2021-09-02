ceedeff388a4ae4712d867db0f7c02ef40803938
minjun8946 <minjun8946@daum.net>
2021-09-02 10:36:53 +0900

    ⚡️:: connect recyclerview

--- presentation/src/main/java/com/example/stats/viewmodel/StatsViewModel.kt
+++ presentation/src/main/java/com/example/stats/viewmodel/StatsViewModel.kt
@@ -1,10 +1,17 @@
 package com.example.stats.viewmodel
 
+import android.widget.BaseAdapter
 import com.example.domain.base.Result
 import com.example.domain.entity.PlayerStatsInfo
 import com.example.domain.usecase.GetPlayerStatsUseCase
+import com.example.stats.BR
+import com.example.stats.R
+import com.example.stats.adapter.RecyclerItem
+import com.example.stats.adapter.RecyclerViewAdapter
 import com.example.stats.base.BaseViewModel
+import com.example.stats.model.BasicModel
 import com.example.stats.model.GameIdModel
+import com.example.stats.model.StatsModel
 import com.example.stats.model.toEntity
 import io.reactivex.observers.DisposableSingleObserver
 import retrofit2.HttpException
@@ -13,29 +20,48 @@ class StatsViewModel(
     private val getPlayerStatsUseCase: GetPlayerStatsUseCase
 ) : BaseViewModel() {
 
-    fun getStats(gameId: GameIdModel){
+    val statsAdapter = RecyclerViewAdapter()
+    var gameStatsModel = ArrayList<StatsModel>()
 
-        val disposableSingleObserver = object : DisposableSingleObserver<Result<PlayerStatsInfo>>(){
-            override fun onSuccess(t: Result<PlayerStatsInfo>) {
+    fun getStats(gameId: GameIdModel) {
 
-                when(t){
-                    is Result.Success ->println("$t")
-                    is Result.Error -> {
-                        onErrorData(t)
+        val disposableSingleObserver =
+            object : DisposableSingleObserver<Result<PlayerStatsInfo>>() {
+                override fun onSuccess(t: Result<PlayerStatsInfo>) {
+
+                    when (t) {
+                        is Result.Success -> {
+
+                            gameStatsModel =
+                                t.response.data.filter { it.minute != "0:00" && it.minute != null }
+                                    .map { it.toEntity() } as ArrayList<StatsModel>
+                            if (gameStatsModel[0].gameInfo.homeTeamId > gameStatsModel[0].gameInfo.visitorTeamId)
+                                gameStatsModel.sortByDescending { it.playerInfo.teamId }
+                            else gameStatsModel.sortBy { it.playerInfo.teamId }
+                            statsAdapter.addData(gameStatsModel.map { it.toRecyclerItem() })
+                        }
+                        is Result.Error -> {
+                            onErrorData(t)
+                        }
+                    }
+                }
+
+                override fun onError(e: Throwable) {
+                    when (e) {
+                        is HttpException -> {
+                            println(e.message())
+                        }
+                        else -> println(e.printStackTrace())
                     }
                 }
             }
-            override fun onError(e: Throwable) {
-                when(e){
-                    is HttpException ->{
-                        println(e.message())
-                    }
-                    else -> println(e.printStackTrace())
-                }
-            }
-        }
         execute(gameId.toEntity(), disposableSingleObserver, getPlayerStatsUseCase)
-
     }
 
+    fun StatsModel.toRecyclerItem() =
+        RecyclerItem(
+            data = this,
+            variableId = BR.repo,
+            layoutId = R.layout.item_game_player
+        )
 }
