522025b807d520abb300482523832ca0164645d9
minjun8946 <minjun8946@daum.net>
2021-09-03 16:17:39 +0900

    ⚡️:: team model

---
+++ presentation/src/main/java/com/example/stats/model/StatsTeamModel.kt
@@ -0,0 +1,26 @@
+package com.example.stats.model
+
+import com.example.domain.entity.StatsTeam
+import com.example.stats.base.teamImage
+import java.io.Serializable
+
+data class StatsTeamModel(
+    val teamId : Int,
+    val abbreviation : String,
+    val city : String,
+    val conference : String,
+    val division : String,
+    val fullName: String,
+    val name: String
+) : Serializable
+
+fun StatsTeam.toEntity() =
+    StatsTeamModel(
+        teamId = teamImage(teamId),
+        abbreviation = abbreviation,
+        city = city,
+        conference = conference,
+        division = division,
+        fullName = fullName,
+        name = name
+    )
