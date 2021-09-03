79e081e19789065fe209f430a35b67634eff5cba
minjun8946 <minjun8946@daum.net>
2021-09-03 16:19:08 +0900

    ⚡️:: add info

--- presentation/src/main/java/com/example/stats/model/StatsGameModel.kt
+++ presentation/src/main/java/com/example/stats/model/StatsGameModel.kt
@@ -1,18 +1,25 @@
 package com.example.stats.model
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.example.domain.entity.StatsGame
+import com.example.stats.base.koreanDay
+import java.io.Serializable
 
 data class StatsGameModel(
     val homeTeamId : Int,
     val homeTeamScore : Int,
     val visitorTeamId : Int,
-    val visitorTeamScore : Int
-)
+    val visitorTeamScore : Int,
+    val gameDate : String
+) : Serializable
 
+@RequiresApi(Build.VERSION_CODES.O)
 fun StatsGame.toEntity() =
     StatsGameModel(
         homeTeamId = homeTeamId,
         homeTeamScore = homeTeamScore,
         visitorTeamId = visitorTeamId,
-        visitorTeamScore = visitorTeamScore
+        visitorTeamScore = visitorTeamScore,
+        gameDate = koreanDay(date.substring(0,10))
     )
