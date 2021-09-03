4f767f46e3dd855380e6dc4161bd1d85038be1d4
minjun8946 <minjun8946@daum.net>
2021-09-03 16:15:24 +0900

    ⚡️:: team game list

---
+++ presentation/src/main/java/com/example/stats/ui/TeamGameListActivity.kt
@@ -0,0 +1,11 @@
+package com.example.stats.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TeamGameListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_team_game_list)
+    }
+}
