62189a6f1c937afef70d404d6588738f59154fce
minjun8946 <minjun8946@daum.net>
2021-09-03 16:20:01 +0900

    ⚡️:: statsmodel

--- presentation/src/main/java/com/example/stats/model/StatsModel.kt
+++ presentation/src/main/java/com/example/stats/model/StatsModel.kt
@@ -1,16 +1,58 @@
 package com.example.stats.model
 
 import com.example.domain.entity.PlayerStats
+import com.example.domain.entity.StatsGame
+import com.example.domain.entity.StatsPlayer
+import com.example.domain.entity.StatsTeam
+import java.io.Serializable
 
 data class StatsModel(
     val point : String,
+
     val assist : String,
+
     val rebound : String,
+
     val steal : Int,
+
     val block : Int,
+
+    val gamesId: Int,
+
+    val deffensiveRebound : Int,
+
+    val threePointSuccessRate : Double,
+
+    val threePointGoalAttempted: Int,
+
+    val threePointGoalMade: Int,
+
+    val fieldGoalSuccessRate : Double,
+
+    val fieldGoalAttempted : Int,
+
+    val fieldGoalMade: Int,
+
+    val freeThrowSuccessRate : Double,
+
+    val freeThrowAttempted: Int,
+
+    val freeThrowMade: Int,
+
+    val minute: String?,
+
+    val offenceRebound: Int,
+
+    val personalFoul: Int,
+
+    val teamInfo: StatsTeamModel,
+
+    val turnover : Int,
+
     val gameInfo : StatsGameModel,
-    val playerInfo : StatsPlayerModel
-)
+
+    val playerInfo : StatsPlayerModel,
+) : Serializable
 
 fun PlayerStats.toEntity() =
     StatsModel(
@@ -19,6 +61,22 @@ fun PlayerStats.toEntity() =
         rebound = " $rebound rebound",
         steal = steal,
         block = block,
+        gamesId = gamesId,
+        deffensiveRebound = deffensiveRebound,
+        threePointSuccessRate = threePointSuccessRate,
+        threePointGoalAttempted = threePointGoalAttempted,
+        threePointGoalMade = threePointGoalMade,
+        fieldGoalSuccessRate = fieldGoalSuccessRate,
+        fieldGoalAttempted = fieldGoalAttempted,
+        fieldGoalMade = fieldGoalMade,
+        freeThrowSuccessRate = freeThrowSuccessRate,
+        freeThrowAttempted = freeThrowAttempted,
+        freeThrowMade = freeThrowMade,
+        minute = minute,
+        offenceRebound = offenceRebound,
+        personalFoul = personalFoul,
+        teamInfo = teamInfo.toEntity(),
+        turnover = turnover,
         gameInfo = gameInfo.toEntity(),
         playerInfo = playerInfo.toEntity()
     )
--- presentation/src/main/java/com/example/stats/model/StatsPlayerModel.kt
+++ presentation/src/main/java/com/example/stats/model/StatsPlayerModel.kt
@@ -2,16 +2,19 @@ package com.example.stats.model
 
 import com.example.domain.entity.StatsPlayer
 import com.example.stats.base.teamImage
+import java.io.Serializable
 
 data class StatsPlayerModel(
     val playerId :Int,
     val teamName : String,
-    val teamId : Int
-)
+    val teamId : Int,
+    val position : String
+) : Serializable
 
 fun StatsPlayer.toEntity() =
     StatsPlayerModel(
         playerId = playerId,
         teamName = lastName,
-        teamId = teamImage(teamId.toInt())
+        teamId = teamImage(teamId.toInt()),
+        position = position
     )
