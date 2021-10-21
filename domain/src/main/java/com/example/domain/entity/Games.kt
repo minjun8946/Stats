a49cf4eda01d026685b0002a4f8e11559c8f13d2
minjun8946 <minjun8946@daum.net>
2021-10-21 20:20:09 +0900

    ⚡️:: add status

--- domain/src/main/java/com/example/domain/entity/Games.kt
+++ domain/src/main/java/com/example/domain/entity/Games.kt
@@ -6,7 +6,9 @@ data class Games(
     val gameId : Int,
     val homeTeamScore : Int,
     val visitorTeamScore : Int,
+    val season : Int,
     val period : Int,
+    val status : String,
     val postSeason : Boolean,
     val visitorTeam: VisitorTeam,
     val homeTeam: HomeTeam
