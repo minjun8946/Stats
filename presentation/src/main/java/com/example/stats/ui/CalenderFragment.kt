0d84ff0e547e10cbd85abf80172d253921be4330
minjun8946 <minjun8946@daum.net>
2021-11-01 10:51:44 +0900

    🐛:: fix xml

--- presentation/src/main/java/com/example/stats/ui/CalenderFragment.kt
+++ presentation/src/main/java/com/example/stats/ui/CalenderFragment.kt
@@ -6,21 +6,41 @@ import com.example.stats.databinding.FragmentCalenderBinding
 import com.example.stats.model.DateModel
 import com.example.stats.viewmodel.CalenderViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.threeten.bp.LocalDate
 
 class CalenderFragment : BaseFragment<FragmentCalenderBinding, CalenderViewModel>() {
 
     override val viewModel: CalenderViewModel by viewModel()
     override val layoutId = R.layout.fragment_calender
+    var koreaTime: LocalDate = LocalDate.now()
+    var day: Int? = null
 
     override fun init() {
         onCalenderClick()
+        binding.reloadBtn.setOnClickListener {
+            getDayGameData()
+        }
     }
 
     private fun onCalenderClick() {
         binding.cvCalendar.setOnDateChangedListener { widget, date, selected ->
-            val koreaTime = date.date.minusDays(1)
-            viewModel.getGamesData(DateModel("${koreaTime.year}-${koreaTime.month.value}-${date.day - 1}",null,null,0,100))
+            koreaTime = date.date.minusDays(1)
+            day = date.day
+            getDayGameData()
         }
+    }
+
+    private fun getDayGameData() {
+        if (day == null) return
+        viewModel.getGamesData(
+            DateModel(
+                "${koreaTime.year}-${koreaTime.month.value}-${day!! - 1}",
+                null,
+                null,
+                0,
+                100
+            )
+        )
     }
 
 }
