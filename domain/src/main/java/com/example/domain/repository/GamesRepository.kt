039c6180884184541d51b43d712fc22cc1b7191f
minjun8946 <minjun8946@daum.net>
2021-12-13 23:24:32 +0900

    ⚡️:: GamesRepository

--- domain/src/main/java/com/example/domain/repository/GamesRepository.kt
+++ domain/src/main/java/com/example/domain/repository/GamesRepository.kt
@@ -1,10 +1,16 @@
 package com.example.domain.repository
 
 import com.example.domain.entity.Date
+import com.example.domain.entity.GameResult
 import com.example.domain.entity.GamesInfo
+import io.reactivex.Completable
 import io.reactivex.Single
 
 interface GamesRepository {
 
     fun getGamesData(date : Date) : Single<GamesInfo>
+
+    fun insertGame(gameResult : List<GameResult>) : Completable
+
+    fun getGameResult() : Single<List<GameResult>>
 }
