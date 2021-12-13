1ab3f4468ca84ac86e18dc78f822c55fae237f64
minjun8946 <minjun8946@daum.net>
2021-12-13 23:20:15 +0900

    ⚡️:: DataSourceImpl

--- data/src/main/java/com/example/data/datasource/GamesDataSourceImpl.kt
+++ data/src/main/java/com/example/data/datasource/GamesDataSourceImpl.kt
@@ -2,11 +2,15 @@ package com.example.data.datasource
 
 import com.example.data.entity.DateData
 import com.example.data.entity.GamesInfoResponse
+import com.example.data.local.GameResultDao
+import com.example.data.local.GameResultEntity
 import com.example.data.remote.ApiService
+import io.reactivex.Completable
 import io.reactivex.Single
 
 class GamesDataSourceImpl(
-    private val apiService: ApiService
+    private val apiService: ApiService,
+    private val gameResultDao: GameResultDao
 ) : GamesDataSource {
     override fun getGamesData(date: DateData): Single<GamesInfoResponse> =
         apiService.getGameData(
@@ -16,5 +20,11 @@ class GamesDataSourceImpl(
             page = date.page,
             perPage = date.perPage,
         )
+
+    override fun insertGame(gameResult: List<GameResultEntity>): Completable =
+        gameResultDao.insertGame(gameResult)
+
+    override fun getGameResult(): Single<List<GameResultEntity>> =
+        gameResultDao.getGameResult()
 
 }
