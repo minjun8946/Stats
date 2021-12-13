5134c41f0f3b942f30918b03ee8d1e4b8c0f7951
minjun8946 <minjun8946@daum.net>
2021-12-13 23:23:52 +0900

    ⚡️:: GamesRepositoryImpl

--- data/src/main/java/com/example/data/repository/GamesRepositoryImpl.kt
+++ data/src/main/java/com/example/data/repository/GamesRepositoryImpl.kt
@@ -2,9 +2,12 @@ package com.example.data.repository
 
 import com.example.data.datasource.GamesDataSource
 import com.example.data.entity.toEntity
+import com.example.data.local.toEntity
 import com.example.domain.entity.Date
+import com.example.domain.entity.GameResult
 import com.example.domain.entity.GamesInfo
 import com.example.domain.repository.GamesRepository
+import io.reactivex.Completable
 import io.reactivex.Single
 
 class GamesRepositoryImpl(
@@ -12,4 +15,10 @@ class GamesRepositoryImpl(
 ) : GamesRepository {
     override fun getGamesData(date: Date): Single<GamesInfo> =
         dataSource.getGamesData(date.toEntity()).map { it.toEntity() }
+
+    override fun insertGame(gameResult: List<GameResult>): Completable =
+        dataSource.insertGame(gameResult.map { it.toEntity() })
+
+    override fun getGameResult(): Single<List<GameResult>> =
+        dataSource.getGameResult().map { it -> it.map { it.toEntity() } }
 }
