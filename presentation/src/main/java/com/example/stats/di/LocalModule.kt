b613088e7c5216bca508ac163f79553b63fd70ec
minjun8946 <minjun8946@daum.net>
2021-12-13 23:26:38 +0900

    ⚡️:: Room Module

---
+++ presentation/src/main/java/com/example/stats/di/LocalModule.kt
@@ -0,0 +1,31 @@
+package com.example.stats.di
+
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.data.local.GameDataBase
+import com.example.data.local.GameResultDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object LocalModule {
+
+    @Singleton
+    @Provides
+    fun provideGameDataBase(
+        @ApplicationContext context: Context
+    ): GameDataBase = Room
+        .databaseBuilder(context, GameDataBase::class.java, "gameDataBase")
+        .build()
+
+    @Singleton
+    @Provides
+    fun provideGameDao(gameResultDatabase: GameDataBase): GameResultDao = gameResultDatabase.gameDao()
+
+}
