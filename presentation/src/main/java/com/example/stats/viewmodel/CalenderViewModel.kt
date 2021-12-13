7677e0508454f7d2fe5b3d6b8578b00f7b05e430
minjun8946 <minjun8946@daum.net>
2021-12-13 23:28:09 +0900

    ⚡️:: test Room

--- presentation/src/main/java/com/example/stats/viewmodel/CalenderViewModel.kt
+++ presentation/src/main/java/com/example/stats/viewmodel/CalenderViewModel.kt
@@ -1,8 +1,13 @@
 package com.example.stats.viewmodel
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.example.domain.base.Result
+import com.example.domain.entity.GameResult
 import com.example.domain.entity.GamesInfo
+import com.example.domain.usecase.GetGameResultUseCase
 import com.example.domain.usecase.GetGamesDataUseCase
+import com.example.domain.usecase.InsertGameDataUseCase
 import com.example.stats.BR
 import com.example.stats.R
 import com.example.stats.adapter.RecyclerItem
@@ -11,6 +16,7 @@ import com.example.stats.base.BaseViewModel
 import com.example.stats.model.DateModel
 import com.example.stats.model.GamesModel
 import com.example.stats.model.toEntity
+import com.example.stats.model.toGameResult
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.reactivex.observers.DisposableSingleObserver
 import javax.inject.Inject
@@ -18,20 +24,53 @@ import javax.inject.Inject
 @HiltViewModel
 class CalenderViewModel @Inject constructor(
     private val getGamesDataUseCase : GetGamesDataUseCase,
+    private val insertGameDataUseCase: InsertGameDataUseCase,
+    private val getGameResultUseCase: GetGameResultUseCase
 ) : BaseViewModel() {
 
     var gamesData = ArrayList<GamesModel>()
     var gameListAdapter = RecyclerViewAdapter()
 
+    fun insertGameData(data : List<GameResult>){
+        val disposableSingleObserver = object : DisposableSingleObserver<Result<Unit>>(){
+            override fun onSuccess(t: Result<Unit>) {
+                println("성공")
+                getGameResult()
+            }
+
+            override fun onError(e: Throwable) {
+                println("실패")
+            }
+        }
+        execute(data,disposableSingleObserver,insertGameDataUseCase)
+
+    }
+
+    fun getGameResult(){
+        val disposableSingleObserver = object : DisposableSingleObserver<Result<List<GameResult>>>(){
+            override fun onSuccess(t: Result<List<GameResult>>) {
+                println("room success $t")
+            }
+
+            override fun onError(e: Throwable) {
+                println("실패")
+            }
+
+        }
+        execute(Unit,disposableSingleObserver,getGameResultUseCase)
+    }
+
     fun getGamesData(date : DateModel){
 
         val disposableObserver = object : DisposableSingleObserver<Result<GamesInfo>>(){
+            @RequiresApi(Build.VERSION_CODES.O)
             override fun onSuccess(t: Result<GamesInfo>) {
                 when(t){
                     is Result.Success -> {
                         gamesData = t.response.data.filter { it.visitorTeamScore != 0 }.map { it.toEntity() } as ArrayList<GamesModel>
                         gameListAdapter.changeData(gamesData.map { it.toRecyclerItem() })
                         println(gamesData)
+                        insertGameData(gamesData.map { it.toGameResult() })
                     }
                     is Result.Error -> println(t.response)
                 }
