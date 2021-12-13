b3bd555c4a6b5a4995cdd19cf385e724ac7929f0
minjun8946 <minjun8946@daum.net>
2021-12-13 23:19:49 +0900

    ⚡️:: GameDataSource

--- data/src/main/java/com/example/data/datasource/GamesDataSource.kt
+++ data/src/main/java/com/example/data/datasource/GamesDataSource.kt
@@ -2,8 +2,14 @@ package com.example.data.datasource
 
 import com.example.data.entity.DateData
 import com.example.data.entity.GamesInfoResponse
+import com.example.data.local.GameResultEntity
+import io.reactivex.Completable
 import io.reactivex.Single
 
 interface GamesDataSource {
     fun getGamesData(date : DateData) : Single<GamesInfoResponse>
+
+    fun insertGame(gameResult : List<GameResultEntity>) : Completable
+
+    fun getGameResult() : Single<List<GameResultEntity>>
 }
