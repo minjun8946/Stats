7a27f803fca46de141858673f20948fb7622b8b0
minjun8946 <minjun8946@daum.net>
2021-12-13 23:24:44 +0900

    ⚡️:: GamesService

--- domain/src/main/java/com/example/domain/service/GamesService.kt
+++ domain/src/main/java/com/example/domain/service/GamesService.kt
@@ -2,10 +2,16 @@ package com.example.domain.service
 
 import com.example.domain.base.Result
 import com.example.domain.entity.Date
+import com.example.domain.entity.GameResult
 import com.example.domain.entity.GamesInfo
+import io.reactivex.Completable
 import io.reactivex.Single
 
 interface GamesService {
 
     fun getGamesData(date : Date) : Single<Result<GamesInfo>>
+
+    fun insertGame(gameResult : List<GameResult>) : Single<Result<Unit>>
+
+    fun getGameResult() : Single<Result<List<GameResult>>>
 }
